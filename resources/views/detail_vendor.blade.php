0b4355a63d3fdae5e322704dbea4009f636ed1cd
muhammadaufazaikra <aufa6@mhs.usk.ac.id>
2026-06-03 22:25:53 +0700

    fix: handle null address on vendor detail page - prevent 500 error for new vendors

--- resources/views/detail_vendor.blade.php
+++ resources/views/detail_vendor.blade.php
@@ -114,7 +114,11 @@
                         <svg class="w-6 h-6 text-red-500 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                         Alamat Lengkap
                     </h4>
+                    @if($vendor->address)
                     <p class="text-gray-600 text-lg ml-9">{{ $vendor->address->jalan }}, {{ $vendor->address->desa }}, {{ $vendor->address->kecamatan }}</p>
+                    @else
+                    <p class="text-gray-500 text-lg ml-9 italic">Belum ada alamat. Vendor sedang melengkapi profil.</p>
+                    @endif
                 </div>
 
                 <!-- Pricelist Card -->
