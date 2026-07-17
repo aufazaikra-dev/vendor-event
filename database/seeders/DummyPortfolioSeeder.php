26e7c7fea75a1cc19f13af7f419a614a5f7611e6
muhammadaufazaikra <aufa6@mhs.usk.ac.id>
2026-07-17 15:54:54 +0700

    feat: update  DummyPortfolioSeeder

--- database/seeders/DummyPortfolioSeeder.php
+++ database/seeders/DummyPortfolioSeeder.php
@@ -30,7 +30,7 @@ class DummyPortfolioSeeder extends Seeder
                 'https://images.unsplash.com/photo-1478146059778-26028b07395a?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1464366400600-7168b8af9bc3?w=800&h=600&fit=crop',
-                'https://images.unsplash.com/photo-1591604329141-fb4ad7a3792a?w=800&h=600&fit=crop',
+                'https://images.unsplash.com/photo-1511795409834-ef04bbd61622?w=800&h=600&fit=crop',
             ],
             'Makeup Artist (MUA)' => [
                 'https://images.unsplash.com/photo-1487412947147-5cebf100ffc2?w=800&h=600&fit=crop',
@@ -47,14 +47,14 @@ class DummyPortfolioSeeder extends Seeder
                 'https://images.unsplash.com/photo-1554048612-b6a482bc67e5?w=800&h=600&fit=crop',
             ],
             'Hiburan (Band/DJ)' => [
-                'https://images.unsplash.com/photo-1501386761578-eaa54b792e25?w=800&h=600&fit=crop',
+                'https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=800&h=600&fit=crop',
             ],
             'MC' => [
-                'https://images.unsplash.com/photo-1560523159-4a9692d222f9?w=800&h=600&fit=crop',
+                'https://images.unsplash.com/photo-1517457373958-b7bdd4587205?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1475721027785-f74eccf877e2?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1517457373958-b7bdd4587205?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?w=800&h=600&fit=crop',
@@ -63,7 +63,7 @@ class DummyPortfolioSeeder extends Seeder
             'Venue/Gedung' => [
                 'https://images.unsplash.com/photo-1519167758481-83f550bb49b3?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1556909114-f6e7ad7d3136?w=800&h=600&fit=crop',
-                'https://images.unsplash.com/photo-1478147427282-58a87a433469?w=800&h=600&fit=crop',
+                'https://images.unsplash.com/photo-1464366400600-7168b8af9bc3?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?w=800&h=600&fit=crop',
                 'https://images.unsplash.com/photo-1464366400600-7168b8af9bc3?w=800&h=600&fit=crop',
             ],
